3582a68ee80835f54ac4097624b37be951a72d70
Igor de Magalhães Victor <igorvictor2002@gmail.com>
2026-04-15 18:41:59 -0300

    Change error messages for user login failures
    Updated error messages for user authentication to indicate incorrect email or password.

--- src/routers/admins.py
+++ src/routers/admins.py
@@ -44,17 +44,17 @@ def login(credenciais: LoginAdmin,db: Session = Depends(get_db)):
     user = get_user_login(db,credenciais.email)
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado!")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Email ou Senha incorretos!")
 
     if not user.ativo:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuário desativado!")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Email ou Senha incorretos!")
 
     if verifica_senha(credenciais.senha,user.senha):
         token = create_token(user.id)
         return {"usuario": user.nome,
                 "token": {"access_token": token, "token_type": "bearer"}}
     else:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Senha incorreta!")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Email ou Senha incorretos")
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED,response_model=AdminResponse)
@@ -120,8 +120,6 @@ def delete_admin(id: int, db: Session = Depends(get_db), payload: dict = Depends
     if not deleted_user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Usuário não encontrado!")
 
-    if not deleted_user.ativo:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail="Usuário já deletado!")
 
     deleted_user.ativo = False
     db.commit()
