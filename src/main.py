60362cdd19e6867405808ec93339cfcd713316bf
Igor de Magalhães Victor <igorvictor2002@gmail.com>
2026-04-15 18:53:57 -0300

    Restrict CORS origins to specific domains

--- src/main.py
+++ src/main.py
@@ -11,7 +11,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://igorvictor23.github.io",
+        "http://localhost:5500"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
