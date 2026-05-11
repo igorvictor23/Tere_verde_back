05bbb2fe9be8da6b416f7d170bd55db27c02cf07
Igor <igorvictor2002@gmail.com>
2026-05-10 21:07:20 -0300

    fix: mudanças nos status http

--- src/routers/admins.py
+++ src/routers/admins.py
@@ -44,17 +44,17 @@ def login(credenciais: LoginAdmin,db: Session = Depends(get_db)):
     user = get_user_login(db,credenciais.email)
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado!")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Email ou Senha incorreta!")
 
     if not user.ativo:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuário desativado!")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Email ou Senha incorreta!")
 
     if verifica_senha(credenciais.senha,user.senha):
         token = create_token(user.id)
         return {"usuario": user.nome,
                 "token": {"access_token": token, "token_type": "bearer"}}
     else:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Senha incorreta!")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Email ou Senha incorreta!")
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED,response_model=AdminResponse)
